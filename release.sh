ffbc5305a06500b843333cc2cea6f9bde6b70eed
ikrong <contact@ikrong.com>
2023-12-19 22:41:02 +0800

    修改脚本

--- release.sh
+++ release.sh
@@ -1,3 +1,7 @@
 git remote add github git@github.com:flextab/widgets.git
 git push github master --force
 git remote remove github
+
+git remote add gitee git@gitee.com:flextab/widgets.git
+git push gitee master --force
+git remote remove gitee
